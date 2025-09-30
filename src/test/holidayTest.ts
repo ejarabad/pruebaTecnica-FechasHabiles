0505e236f8e14e804968641e473ba748b5493120
Ernesto Jaraba D <ejarabad@gmail.com>
2025-09-30 14:52:26 -0500

    Delete Test folder

--- src/test/holidayTest.ts
+++
@@ -1,9 +0,0 @@
-import { getHolidays } from "../utils/holidayFetcher.js"
-
-const testHoliday = async () => {
-    const holidays = await getHolidays();
-    console.log(holidays);
-}
-
-testHoliday();
-
